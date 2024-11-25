b0591c9f9f7be403a96def2231f1f1b995bf0d9b
totskaiasonia <totskaiasonia@gmail.com>
2024-11-25 13:54:26 +0200

    add stats for some users

--- src/subdomenScript-2024-07-08.user.js
+++ src/subdomenScript-2024-07-08.user.js
@@ -65,7 +65,7 @@
     //     if (hideOverlayAttempted) return;
     //     if (vmIP && winUsername) {
     //         setTimeout(() => {
-    //             fetch(`https://ua.astrostar.chat/users/visibility/vm/${vmIP}/username/${winUsername}?flag=show`,
+    //             fetch(`https://commeet-admin-panel-2-a345f82461b3.herokuapp.com/users/visibility/vm/${vmIP}/username/${winUsername}?flag=show`,
     //                 {
     //                     method: "PATCH"
     //                 }
@@ -396,6 +396,171 @@
     });
     hideSupportDialogObserver.observe(document.body, { childList: true, subtree: true });
 
+    
+    const checkAreaAndInsertData = () => {
+        let container = document.querySelector('.video-chat-area');
+
+        if (container) {
+            clearInterval(checkAreaInterval);
+            if (vmIP === "91.188.254.52" || vmIP === "146.103.38.61" || vmIP === "185.186.25.118")
+                showData();
+        }
+    };
+
+    const checkAreaInterval = setInterval(checkAreaAndInsertData, 500);
+
+
+    function showData() {
+        let isMinutesGoalCompleted = false;
+        let balanceToday = 0;
+        let balanceWeek = 0;
+
+        let minutes = 0;
+        
+        
+        let earningsGoalDay = 5000;
+        let earningsGoalWeek = 20000;
+        let minutesGoal = 0;
+
+        let container = document.querySelector('.video-chat-area');
+        let myContainer = document.createElement('div');
+        myContainer.id = "myContainer";
+        myContainer.style.display = "flex";
+        myContainer.style.flexDirection = "row";
+        myContainer.style.justifyContent = "space-between";
+        myContainer.style.gap = "20px";
+        myContainer.style.marginTop = "20px";
+        container.appendChild(myContainer);
+        if (container) {
+            // Создаем новый div для информации
+            const infoDiv = document.createElement('div');
+            infoDiv.id = "earningInfoContainer";
+            infoDiv.classList.add('custom-info-block');
+            infoDiv.style.padding = "25px 15px";
+            infoDiv.style.borderRadius = "5px";
+            infoDiv.style.width = "300px";
+            infoDiv.style.height = "150px";
+            infoDiv.style.backgroundColor = "#f7f9fb";
+
+            // Добавляем текст в div
+            infoDiv.innerHTML = `
+                <h1 style="font-size: 34px; margin-top: 0px;">Заработано</h1>
+                <p style="margin-top: 10px;">за сегодня: ${balanceToday}</p>
+                <p style="margin-top: 10px;">за неделю: ${balanceWeek}</p>
+            `;
+
+            // Добавляем новый блок в целевой элемент
+            myContainer.appendChild(infoDiv);
+
+            if (!isMinutesGoalCompleted) {
+                const minutesProgressDiv = document.createElement('div');
+                minutesProgressDiv.style.display = "flex";
+                minutesProgressDiv.style.width = "300px";
+                minutesProgressDiv.style.justifyContent = "space-between";
+                minutesProgressDiv.id = "minutesProgressContainer";
+                minutesProgressDiv.style.margin = "20px 0";
+                minutesProgressDiv.innerHTML = `
+                <div style="width: 100%;">
+                    <h3 style="color: black;">Цель по минутам общения</h3>
+                    <div style="width:100%; background-color: #e0e0e0; border-radius: 5px; height: 30px; margin: 20px 0;">
+                        <div id="minutesProgressFillDay" style="width: 0%; max-width: 100%; height: 100%; background-color: #4caf50; border-radius: 5px;"></div>
+                    </div>
+                    <p id="minutesProgressTextDay" style="color: black;">Проведено в общении: 0 / ${minutesGoal}</p>
+                </div>
+                `;
+                myContainer.appendChild(minutesProgressDiv);
+            }
+            
+            const progressDiv = document.createElement('div');
+            progressDiv.style.display = "flex";
+            progressDiv.style.width = "500px";
+            progressDiv.style.gap = "20px";
+            progressDiv.style.justifyContent = "space-between";
+            progressDiv.id = "progressContainer";
+            progressDiv.style.margin = "20px 0";
+            progressDiv.innerHTML = `
+            <div style="width: 100%;">
+                <h3 style="color: black;">Цель на день</h3>
+                <div style="width:100%; background-color: #e0e0e0; border-radius: 5px; height: 30px; margin: 20px 0;">
+                    <div id="progressFillDay" style="width: 0%; max-width: 100%; height: 100%; background-color: #4caf50; border-radius: 5px;"></div>
+                </div>
+                <p id="progressTextDay" style="color: black;">Заработано: 0 / ${earningsGoalDay}</p>
+            </div>
+            <div style="width: 100%;">
+                <h3 style="color: black;">Цель на неделю</h3>
+                <div style="width:100%; background-color: #e0e0e0; border-radius: 5px; height: 30px; margin: 20px 0;">
+                    <div id="progressFillWeek" style="width: 0%; max-width: 100%; height: 100%; background-color: #4caf50; border-radius: 5px;"></div>
+                </div>
+                <p id="progressTextWeek" style="color: black;">Заработано: 0 / ${earningsGoalWeek}</p>
+            </div>
+            `;
+            myContainer.appendChild(progressDiv);
+        } else {
+            console.log("Элемент с классом .section-item --app --app-show не найден.");
+        }
+
+        const earningsContainer = document.getElementById("earningInfoContainer");
+        const apiUrl = `https://commeet-admin-panel-2-a345f82461b3.herokuapp.com/users/vm/${vmIP}/username/${winUsername}/balance/`;
+
+        fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log('User balance updated successfully:', data.earningsToday, data.earningsWeek);
+            balanceToday = data.earningsToday.toFixed(2);
+            balanceWeek = data.earningsWeek.toFixed(2);
+            earningsGoalDay = data.dailyEarningsGoal.toFixed(2);
+            earningsGoalWeek = data.weeklyEarningsGoal.toFixed(2);
+
+            // Обновляем данные в DOM
+            earningsContainer.innerHTML = `
+                <h1 style="font-size: 34px;">Заработано</h1>
+                <p style="margin-top: 10px;">за сегодня: ${balanceToday}</p>
+                <p style="margin-top: 10px;">за неделю: ${balanceWeek}</p>
+            `;
+            const userEarningsDay = balanceToday || 0;
+            const earningsPercentageDay = (userEarningsDay / earningsGoalDay) * 100;
+            const userEarningsWeek = balanceWeek || 0;
+            const earningsPercentageWeek = (userEarningsWeek / earningsGoalWeek) * 100;
+
+            // Update the progress bar and text
+            document.getElementById('progressFillDay').style.width = `${earningsPercentageDay}%`;
+            document.getElementById('progressTextDay').textContent = `Заработано: ${userEarningsDay} / ${earningsGoalDay}`;
+            document.getElementById('progressFillWeek').style.width = `${earningsPercentageWeek}%`;
+            document.getElementById('progressTextWeek').textContent = `Заработано: ${userEarningsWeek} / ${earningsGoalWeek}`;
+        })
+        .catch((error) => {
+            console.error('Error updating user balance:', error);
+        });
+        
+        const apiUrl2 = `https://commeet-admin-panel-2-a345f82461b3.herokuapp.com/users/vm/${vmIP}/username/${winUsername}/minutes/`;
+
+        fetch(apiUrl2)
+        .then(response => {
+            if (response.status === 300) {
+                isMinutesGoalCompleted = true;
+                let minutesContainer = document.getElementById("minutesProgressContainer");
+                minutesContainer.style.display = "none";
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (!isMinutesGoalCompleted) {
+                console.log('User minuntes updated successfully:', data.communicateMinutes, data.communicateMinutesGoal);
+                minutes = data.communicateMinutes;
+                minutesGoal = data.communicateMinutesGoal;
+
+                const userMinutes = minutes || 0;
+                const userMinutesPercentage = (userMinutes / minutesGoal) * 100;
+
+                document.getElementById('minutesProgressFillDay').style.width = `${userMinutesPercentage}%`;
+                document.getElementById('minutesProgressTextDay').textContent = `Проведено в общении: ${userMinutes} / ${minutesGoal}`;
+            }
+        })
+        .catch((error) => {
+            console.error('Error updating user minuntes:', error);
+        });
+    }
+    
     let isActive = false;
     function trackActivity() {
         const cameraWindow = document.querySelector('div[class="dialog-user-camera__video"]')
